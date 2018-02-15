09b1c7cf864dd54c477bad7bebec3637eee3a5f1
lairmartes <lairmartes@gmail.com>
2018-02-15 08:23:54 -0200

    switching to async/await syntax

--- server/server.js
+++ server/server.js
@@ -60,24 +60,32 @@ app.get('/todos/:id', authenticate, (req, res) => {
     });
 });
 
-app.delete('/todos/:id', authenticate, (req, res) => {
-    var id = req.params.id;
-    if (!ObjectID.isValid(id)) {
-        return res.status(404).send();
-    }
+app.delete('/todos/:id', authenticate, async(req, res) => {
 
-    Todo.findOneAndRemove({
-        _id: id,
-        _creator: req.user._id
-    }).then((todo) => {
-        if (!todo) {
-            return res.status(404).send();
+    try {
+        // get the id from request data
+        const id = req.params.id;
+        // return bad request if id sent is not valid (syntatically)
+        if (!ObjectID.isValid(id)) {
+            res.status(404).send();
         }
+        // remove the todo record and return 'undefined' if no record has been found
+        const todo = await Todo.findOneAndRemove({
+            _id: id,
+            _creator: req.user._id
+        });
+        // return bad request if no todo has been found to be removed
+        if (!todo) {
+            res.status(404).send();
+        } /* return the record removed in case of success */
+        else {
 
-        res.send({ todo });
-    }).catch((e) => {
-        res.status(400).send();
-    });
+            res.send({ todo });
+        }
+    } catch (e) {
+        // return a server error in case of something goes wrong during this operation.
+        res.status(500).send();
+    }
 });
 
 app.patch('/todos/:id', authenticate, (req, res) => {
@@ -113,23 +121,21 @@ app.patch('/todos/:id', authenticate, (req, res) => {
 
 /* User services */
 
-app.post('/users', (req, res) => {
-    // getting user data from request
-    var body = _.pick(req.body, ['email', 'password']);
-
-    // create a mongodb user with body data
-    var user = new User(body);
-
-    // try to save
-    user.save().then(() => {
+app.post('/users', async(req, res) => {
+    try {
+        // getting user data from request
+        const body = _.pick(req.body, ['email', 'password']);
+        // create a mongodb user with body data
+        const user = new User(body);
+        // try to save user
+        await user.save();
         // after saving the user, includes token data calling user method
-        return user.generateAuthToken();
-    }).then((token) => {
+        token = await user.generateAuthToken();
         // send token back to the caller
         res.header('x-auth', token).send(user);
-    }).catch((e) => {
+    } catch (e) {
         res.status(400).send(e); // bad request if data is not correct
-    });
+    }
 });
 
 // GET service to get a user by token
@@ -139,24 +145,26 @@ app.get('/users/me', authenticate, (req, res) => {
 });
 
 //POST /users/login {email, password}
-app.post('/users/login', (req, res) => {
-    var body = _.pick(req.body, ['email', 'password']);
+app.post('/users/login', async(req, res) => {
 
-    User.findByCredentials(body.email, body.password).then((user) => {
-        return user.generateAuthToken().then((token) => {
-            res.header('x-auth', token).send(user);
-        });
-    }).catch((e) => {
+    try {
+        const body = _.pick(req.body, ['email', 'password']);
+        const user = await User.findByCredentials(body.email, body.password);
+        const token = await user.generateAuthToken();
+        res.header('x-auth', token).send(user);
+    } catch (e) {
         res.status(400).send();
-    });
+    }
+
 });
 
-app.delete('/users/me/token', authenticate, (req, res) => {
-    req.user.removeToken(req.token).then(() => {
+app.delete('/users/me/token', authenticate, async(req, res) => {
+    try {
+        await req.user.removeToken(req.token);
         res.status(200).send();
-    }, () => {
+    } catch (e) {
         res.status(400).send();
-    });
+    }
 });
 
 app.listen(port, () => {
